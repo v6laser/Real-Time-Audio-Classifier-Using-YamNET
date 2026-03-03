8c629d0ef6d37377836c3f95dc7ae161ba17e521
J Nair <146624328+v6laser@users.noreply.github.com>
2026-03-03 23:14:55 +0530

    main.py

--- main.py
+++ main.py
@@ -7,41 +7,27 @@ import csv
 #pip install tensorflow tensorflow_hub librosa
 print("Loading YAMNet model...")
 model = hub.load("https://tfhub.dev/google/yamnet/1")
-
-# Load class names correctly
 class_map_path = model.class_map_path().numpy()
-
 class_names = []
 with open(class_map_path, newline='') as f:
     reader = csv.reader(f)
-    next(reader)  # skip header
+    next(reader)  
     for row in reader:
-        class_names.append(row[2])  # display_name only
-
-# Get first WAV file in input_audio folder
+        class_names.append(row[2]) 
 input_folder = "input_audio"
 audio_files = [f for f in os.listdir(input_folder) if f.endswith(".wav")]
-
 if not audio_files:
     print("No WAV files found in input_audio folder.")
     exit()
-
 audio_file = os.path.join(input_folder, audio_files[0])
 print(f"\nProcessing file: {audio_files[0]}")
-
-# Load and trim silence
 waveform, sr = librosa.load(audio_file, sr=16000)
 waveform, _ = librosa.effects.trim(waveform)
-
 print("Analyzing audio...")
 scores, embeddings, spectrogram = model(waveform)
-
-# Get top prediction
 mean_scores = tf.reduce_mean(scores, axis=0).numpy()
 top_index = np.argmax(mean_scores)
-
 top_label = class_names[top_index]
 top_confidence = mean_scores[top_index]
-
 print("\nTop Prediction:")
 print(f"{top_label} ({top_confidence:.2f})")
